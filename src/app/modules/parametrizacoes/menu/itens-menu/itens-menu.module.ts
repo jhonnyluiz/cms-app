d02ecc601a973e85323a270d724dc2823b959b21
Jhonny L. Cabral <jhonny.silva@capgemini.com>
2022-02-14 23:57:23 -0300

    feat: modulos

---
+++ src/app/modules/parametrizacoes/menu/itens-menu/itens-menu.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ItensMenuRoutingModule } from './itens-menu-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ItensMenuRoutingModule
+  ]
+})
+export class ItensMenuModule { }
---
+++ src/app/modules/parametrizacoes/operacao/operacao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OperacaoRoutingModule } from './operacao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OperacaoRoutingModule
+  ]
+})
+export class OperacaoModule { }
